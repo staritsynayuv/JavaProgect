cb109981ad7240bb8aed9cd849f392f37f241513
Старицына Юлия <staritzyna.jul@yandex.ru>
2018-03-15 14:30:01 +0300

    Задача 17
    решение

--- Example17.java
+++ Example17.java
@@ -1,40 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Example17 {
     public static void main(String args[]) {
-        int[][] a = new int[6][7];
-        int k, i;
-        for ( i = 0; i < a.length; i++) {
+        Integer [][] a = new Integer [6][7];
+        int i, k;
+        for (  i = 0; i < a.length; i++) {
             for ( k = 0; k < a[i].length; k++) {
                 a[i][k] = (int) (Math.random() * 10);
                 System.out.print(a[i][k] + " ");
-
-                int max = 0, indexmax = 0;
-
-                if (max < a[i][k]) {
-                    max = a[i][k];
-                    indexmax = k;
-                }
-
-
-                int n = a[i][0];
-
-                a[i][0] = a[i][indexmax];
-
-                a[i][indexmax] = n;
-
-
-
             }
-            System.out.println(" ");
-
+            System.out.println();
         }
+
 
         System.out.println("--------------");
 
-        for ( i = 0; i < a.length; i++) {
-            for ( k = 0; k < a[i].length; k++) {
+        for (  i = 0; i < a.length; i++) {
+            Arrays.sort(a[i], Collections.reverseOrder());
+            for (  k = 0; k < a[i].length; k++)
                 System.out.print(a[i][k] + " ");
-            }
-            System.out.println(" ");
+
+            System.out.println();
         }
 
     }
