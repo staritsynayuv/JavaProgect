55bf5bcbd430f9fc5516d208e4adf5e1247f2c75
staritsynayuv <36505287+staritsynayuv@users.noreply.github.com>
2018-03-29 16:29:32 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner in = new Scanner(System.in);
+    static final Office office = new Office();
+    static void fill(){
+
+        int n = in.nextInt();
+
+        for(int i = 0; i< n; i++) {
+            Employee employee = new Employee(in.next(), in.next(), Rank.valueOf(in.nextLine()) );
+
+            office.add(employee);
+        }
+    }
+
+    public static void outInfo() {
+        office.showEmployeeList();
+    }
+
+    public static void find(){
+        String rankName = in.next();
+        Rank rank = Rank.getRank(rankName);
+        if (rank == null) {
+            System.out.println("Неизвестная должность" + rankName);
+        }
+        else {
+
+            for (Employee employee: office.find(rank)) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+
     public static void main(String[] args){
-        Office office = new Office();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Фамилия Имя:");
-        for (int i=1; i<=3; i++) {
-            String F = scanner.next();
-            String N = scanner.next();
-            Employee FN = new Employee(F, N);
-            // Employee petrovPetr   = new Employee( "Петров",  "Пётр");
+        System.out.println("Введите команду");
+        String command = in.next() ;
+        if (command.equals("fill")){
+            fill();
+        }
+
+     //   if (command.equals("outInfo")) {
+       //     outInfo();
+        //}
+
+        // Employee petrovPetr   = new Employee( "Петров",  "Пётр");
             // Employee alecseevAlex = new Employee( "Алексей", "Алексеев");
-            office.add(FN);
-            System.out.println(office.isEmployeeInOffice(FN));
+
            // Employee alecseevAlex = new Employee(120000);
             //System.out.println(office.isEmployeeInOffice(petrovPetr));
             // System.out.println(office.isEmployeeInOffice(alecseevAlex));
-        }
+
       //  office.add(petrovPetr);
       // office.add(alecseevAlex);
-        office.Sort();
+      // office.Sort();
        // office.showEmployeeList();
     }
+
 }
