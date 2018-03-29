61e8f9d35f97f316602ce2658f8c1f8b62ded5f4
staritsynayuv <36505287+staritsynayuv@users.noreply.github.com>
2018-03-29 16:28:06 +0300

    Update Rank.java

--- Rank.java
+++ Rank.java
@@ -1,4 +1,38 @@
 public enum  Rank {
-    ENGINEER();
-}
+    ENGINEER("Инженер"),
+    TESTER("Тестировщик"),
+    DIRECTOR("Директор"),
+    SINIOR_ENGINEER("Старший инженер"),
+    PROGRAMMER("Программист");
 
+
+    Rank(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static boolean isRankExist(String rankName) {
+        for (Rank element : values()) {
+            if (rankName.equals(element.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Rank getRank(String rankName) {
+        for (Rank element : values()) {
+            if (rankName.equals(element.getName())) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+}
